ca6247724dc7cfa275320db1e28c42f943fcb07d
usp <ususso@gmail.com>
2025-03-08 22:42:22 +0900

    Enable checkhealth

--- plugin/pbrowse.lua
+++ plugin/pbrowse.lua
@@ -3,7 +3,6 @@ if vim.g.loaded_pbrowse == 1 or vim.opt.compatible:get() then
 end
 
 vim.g.loaded_pbrowse = 1
-vim.g.loaded_pbrowse_health = 1
 
 vim.api.nvim_create_user_command('PBrowse', function(opts)
   require('pbrowse').browse(opts.line1, opts.line2, opts.range)
