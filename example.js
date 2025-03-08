55e9ebcba6070173b5f5b59fc8c081faf32cd46b
usp <ususso@gmail.com>
2025-03-08 15:29:27 +0900

    feat: add example calculation and greeting functions
    This commit adds a new example.js file to demonstrate pull request
    workflow. The file includes:
    - A calculateSum function for basic arithmetic
    - A simple greeting function
    - Module exports for both functions
    
    For demonstration purposes only.

---
+++ example.js
@@ -0,0 +1,16 @@
+/**
+ * Example file to demonstrate a pull request
+ * 
+ * This file shows how you might make changes in a PR.
+ */
+
+function calculateSum(a, b) {
+  return a + b;
+}
+
+// Before PR: Simple greeting
+function greet(name) {
+  return "Hello " + name;
+}
+
+module.exports = { calculateSum, greet };
